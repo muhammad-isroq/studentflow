6a3e0f0dd664cb9d2783fa88e4277db5039ebee0
Muhammad Isroq <muhammadisroq7@gmail.com>
2025-09-02 16:27:41 +0700

    fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
     use HasRoles;
 
     /**

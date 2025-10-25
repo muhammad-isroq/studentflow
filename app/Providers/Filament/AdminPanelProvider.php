e01fdc384c79d2a586324d7c39bba3aa5a7ece5a
Muhammad Isroq <muhammadisroq7@gmail.com>
2025-10-25 09:38:41 +0700

    mengubah path admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,7 +42,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('studentflow')
             ->authGuard('web')
             ->login()
             
